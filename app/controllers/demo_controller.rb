d0ee4031e448111df263cda9ac99f449f809fe53
David Tamakloe <stdavidgh@gmail.com>
2014-06-20 14:57:36 +0000

    Add database tables

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,5 +3,19 @@ class DemoController < ApplicationController
   layout false
 
   def index
+  	render("index.html.erb")
   end
+
+  def hello
+  	@array = [1, 2, 3, 4, 5]
+  	@id = params[:id].to_i
+  	@page = params["page"].to_i
+  	render("hello.html.erb")
+
+  end
+
+  def other_hello
+    redirect_to(:controller => 'demo', :action => 'index')
+  end
+
 end
